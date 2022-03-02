4f184fb54fb3c23e6598407d58c9504c43674325
MisterPerfect007 <leneodepart@gmail.com>
2022-03-02 16:15:39 +0000

    First commit

---
+++ lib/screens/quiz/quiz.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatelessWidget {
+  const Quiz({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: ,
+      body: Container(),
+    );
+  }
+}
